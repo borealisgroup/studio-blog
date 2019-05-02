ec1bffcaae621ffe712687940f7bd14641a6defa
Lewis Fidlers <lewis.fidlers@borealisgroup.com>
2019-05-02 16:05:22 +0200

    Updates

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// custom typefaces
-import 'typeface-montserrat';
-import 'typeface-merriweather';
-
-// Prism js theme for code highlighting in markdown
-require('prismjs/themes/prism-solarizedlight.css');
