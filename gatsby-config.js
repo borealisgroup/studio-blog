f5c3030e842612166a59cf2a7e68e705ede25104
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-06-13 13:20:08 +0200

    new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
+        icon: 'src/images/favicon.png',
         // trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
